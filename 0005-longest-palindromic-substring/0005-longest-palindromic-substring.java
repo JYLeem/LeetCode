124b62a966de9fa2d538a1040f29469b7c66f5f6
JYLeem <102199863+JYLeem@users.noreply.github.com>
2024-06-09 15:00:17 +0900

    Time: 149 ms (35.93%), Space: 45.5 MB (24.99%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,34 +1,37 @@
+import java.util.*;
+
 class Solution {
-    static String ans;
+    static String ans = "";
     static int cnt = 0;
 
-    public String longestPalindrome(String s) {
+    public static String longestPalindrome(String s) {
         ans = "";
         cnt = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j <= s.length(); j++) {
-                String substring = s.substring(i, j);
-                Checker(substring);
-            }
+            // 홀수 길이 팰린드롬 체크
+            expandAndCheck(s, i, i);
+            // 짝수 길이 팰린드롬 체크
+            expandAndCheck(s, i, i + 1);
         }
+
         return ans;
     }
 
-    public static void Checker(String s) {
-        if (s.length() <= cnt) return;
-
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return;
-            }
-            left++;
-            right--;
+    public static void expandAndCheck(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            Checker(s.substring(left, right + 1));
+            left--;
+            right++;
         }
-        cnt = s.length();
-        ans = s;
+    }
+
+    public static boolean Checker(String s) {
+        if (s.length() > cnt) {
+            cnt = s.length();
+            ans = s;
+            return true;
+        }
+        return false;
     }
 }
