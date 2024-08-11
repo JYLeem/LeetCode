56b818d707a63dddcd9f0dc447e96158f89c6e17
JYLeem <102199863+JYLeem@users.noreply.github.com>
2024-08-11 22:19:54 +0900

    Time: 0 ms (100%), Space: 41.6 MB (61.52%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,15 @@
+import java.util.StringTokenizer;
+
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        StringTokenizer st = new StringTokenizer(s, " ");
+        String temp = "";
+        
+        while(st.hasMoreTokens()){
+             temp = st.nextToken();
+        }
+
+        return temp.length();
+    }
+}
