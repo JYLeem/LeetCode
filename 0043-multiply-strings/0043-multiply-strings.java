319e34e8bcff475a83c66dec51744aa3a3aa6788
JYLeem <102199863+JYLeem@users.noreply.github.com>
2024-08-17 15:24:55 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        int tmp1 = Integer.parseInt(num1);
+        int tmp2 = Integer.parseInt(num2);
+
+        return String.valueOf(tmp1 * tmp2);
+    }
+}
